26e14f2d62330f04ca071972394991299cfa659b
Thales Figueredo Pereira <thalesfp@gmail.com>
2026-04-03 18:12:49 -0700

    remove decorator support and SWC dev dependencies

--- src/react/index.ts
+++ src/react/index.ts
@@ -1,5 +1,4 @@
 export { ReactSnapStore as SnapStore } from "./store.js";
-export { connect, scoped } from "./connect.js";
 export { setHttpClient, setDefaultHeaders } from "../core/base.js";
 export { asyncStatus } from "../core/types.js";
 export type { PickFn } from "./store.js";
